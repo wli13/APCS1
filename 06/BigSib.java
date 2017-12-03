0c004d19a252434b5e4cbfaafe19480a97dbe8fc
wenting li <wli13@stuy.edu>
2017-12-02 20:13:54 -0500

    BigSib FINISHED

--- 06/BigSib.java
+++ 06/BigSib.java
@@ -1,7 +1,7 @@
-/*Wenting Li
-APCS1 pd08
-HW06 -- BigSibg Individuality and the Elimination of Radio Fuzz
-2017-09-26 */
+//Wenting Li
+//APCS1 pd08
+//HW06 -- BigSibg Individuality and the Elimination of Radio Fuzz
+//2017-09-26 
 
 public class BigSib {
      private String HelloMsg;
